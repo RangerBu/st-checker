6c682206becc7f9eec97906c185fb027306e5b81
origin.portable <origin.partable@cname.self>
2018-01-01 20:29:43 +0800

    wpds-builder

---
+++ builder/model-builder/WPDS_builder.h
@@ -0,0 +1,21 @@
+#ifndef WPDS_BUILDER_H
+#define WPDS_BUILDER_H
+
+#include "wali/SemElem.hpp"
+#include "wali/Common.hpp"
+
+#include "../flow-graph-builder/CFG.h"
+
+class WPDS_builder
+{
+    /**
+    * public methods
+    */
+    WPDS_builder(CFG *);
+    virtual ~WPDS_builder();
+
+
+
+};
+
+#endif // WPDS_BUILDER_H
